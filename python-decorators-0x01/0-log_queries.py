69069ed8417df8798699a090274e40e09b9dd69b
Reaganz-Wat <reaganwatmon6@gmail.com>
2025-07-10 10:18:50 +0300

    add loggin to a file including date-time

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,14 +1,21 @@
 import sqlite3
+from datetime import datetime
+
+from reportlab.graphics.barcode.code128 import starta
+from sympy import resultant
+
 
 #### decorator to lof SQL queries
 def log_queries(func):
     def wrapper(*args, **kwargs):
-        print(f"The Query is: {kwargs}")
-
-        return func(*args, **kwargs)
-
-        print("Finished running querry")
-
+        start_time = datetime.now()
+        result = func(*args, **kwargs)
+        end_time = datetime.now()
+        time_taken = end_time - start_time
+        with open('logs.txt', 'a') as log:
+            log.write(f"\n Starting... Datetime: {datetime.now()} \n{datetime.now()} | Query: {kwargs} \n Time taken: {time_taken}\n")
+            print(f"{datetime.now()} | Query: {kwargs} \n Time taken: {time_taken}")
+        return result
     return wrapper
 
 @log_queries
