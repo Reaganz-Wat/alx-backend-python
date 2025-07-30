541a58c95f85133b22189039a4c1b2c31d9b49d6
Reaganz-Wat <reaganwatmon6@gmail.com>
2025-07-30 12:48:24 +0300

    feat: track message edits using pre_save signal and store history in MessageHistory model - Added , , and  fields to  model - Introduced  model to log previous message content - Connected  signal to automatically save old content before update - Enables edit tracking and viewing of message history in UI

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,6 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
 def create_notification_on_new_message(sender, instance, created, **kwargs):
@@ -9,3 +9,23 @@ def create_notification_on_new_message(sender, instance, created, **kwargs):
             user=instance.receiver,
             message=instance
         )
+
+@receiver(pre_save, sender=Message)
+def create_history_on_message_update(sender, instance: Message, **kwargs):
+    if not instance.pk:
+        # This is a new message (not yet in the DB) -> Skip it
+        return
+
+    try:
+        old_instance = Message.objects.get(pk=instance.pk)
+    except Message.DoesNotExist:
+        return # this is for the safety
+
+    if instance.content != old_instance.content:
+        MessageHistory.objects.create(
+            message=old_instance,
+            previous_content=old_instance.content,
+            edited_by=instance.edited_by
+        )
+
+        instance.edited = True
